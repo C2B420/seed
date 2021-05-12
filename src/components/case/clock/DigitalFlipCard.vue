50db2cfae79599894ed59b3ab14eee64a0208cfd
吴奇楠 <804332469@qq.com>
2021-05-13 00:59:20 +0800

    数字翻转卡片组件，样式已完成，翻转动画未完成

---
+++ src/components/case/clock/DigitalFlipCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="card"
+       :style="{
+          'width': width + 'px',
+          'height': height + 'px',
+          'background-color': bgColor,
+          'color': color,
+          'font-size': fontSize + 'px',
+        }">
+    <span :style="{'line-height': height + 'px'}">
+      <slot></slot>
+    </span>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DigitalFlipCard',
+  props: {
+    width: {
+      type: Number,
+      default: 77,
+    },
+    height: {
+      type: Number,
+      default: 127,
+    },
+    bgColor: {
+      type: String,
+      default: '#000000',
+    },
+    color: {
+      type: String,
+      default: '#FFFFFF',
+    },
+    fontSize: {
+      type: Number,
+      default: 90,
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+  .card {
+    position: relative;
+    margin: 3px;
+    border-radius: 12px;
+    font-weight: bolder;
+    text-align: center;
+    .line{
+      .absolute_vertical_center();
+      width: 100%;
+      height: 1px;
+      background-color: gray;
+    }
+  }
+</style>
