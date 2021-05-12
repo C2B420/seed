6370a2e17e878b8f7041a550f74cfa2831f8fcdb
吴奇楠 <804332469@qq.com>
2021-05-12 23:59:33 +0800

    添加卡片翻转组件

---
+++ src/views/case/ClockDisplay210513.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <div class="inner"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ClockDisplay',
+};
+</script>
+
+<style scoped lang="less">
+  .wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-color: pink;
+
+    .inner {
+      .absolute_center();
+      width: 500px;
+      height: 100px;
+      background-color: deeppink;
+    }
+  }
+</style>
