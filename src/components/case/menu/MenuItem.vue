cf777e129cde173985f6aa83232c2b0ce18a824f
吴奇楠 <804332469@qq.com>
2021-05-14 17:03:00 +0800

    添加菜单项组件

---
+++ src/components/case/menu/MenuItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="item">
+    <span>
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuItem',
+};
+</script>
+
+<style scoped lang="less">
+  .item {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    text-align: center;
+    background-color: rgba(255, 255, 255, 0.35);
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.5);
+
+      span {
+        font-size: 35px;
+      }
+    }
+
+    span {
+      color: rgba(255, 255, 255, 0.65);
+      line-height: 60px;
+      font-size: 30px;
+    }
+  }
+</style>
